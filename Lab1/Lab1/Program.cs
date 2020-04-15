7bd4bbf55740a814b644bc2242f3ec3a29f67256
Alexey <djkah11@yandex.ru>
2020-04-15 23:05:40 +0300

    lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string smallStr = "abc";
+            string bigStr = "abcdefghijklpmabceee";
+            int m = smallStr.Length;
+            int n = bigStr.Length;
+            for (int i = 0; i < n - m; ++i)
+            {
+                if (smallStr == bigStr.Substring(i, m))
+                {
+                    Console.WriteLine(String.Format("Образец найден со сдвигом {0}", i));
+                }
+            }
+        }
+    }
+}
